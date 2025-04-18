36eae33d6a920ff6cc70b8590c98e6cbbdb258af
yanartas2007 <yanartas2007@gmail.com>
2025-04-18 14:08:39 +0300

    test2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 import requests
-from telegram.ext import Application, MessageHandler, filters
+from telegram.ext import Application, MessageHandler, filters, CommandHandler
+from valutes import *
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
@@ -10,21 +11,31 @@ logger = logging.getLogger(__name__)
 
 
 async def echo(update, context):
-    adres = 'https://www.cbr-xml-daily.ru/daily_json.js'
-    data = requests.get(adres).json()
-    a = update.message.text
-    s = str(data['Valute'][a]['Name']+str(data['Valute'][a]['Value']))
+    m = update.message.text
     try:
-        await update.message.reply_text(s)
+        data = take_data()
+        await update.message.reply_text(f"{data['Valute'][m]['Name']} курс к рублю {data['Valute'][m]['Value']}")
     except Exception:
-        await update.message.reply_text('напишите название валюты чтобы получить справку. Например:\nUSD, MDL, NZD')
+        await update.message.reply_text("Некорректно введена валюта. Получить справку: /help")
+
+async def start(update, context):
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Привет {user.mention_html()}! Напиши название валюты и узнаешь ее курс к рублю",
+    )
+
+
+async def help_command(update, context):
+    await update.message.reply_text("Введите валюту, например USD")
 
 
 def main():
     application = Application.builder().token('7224928959:AAG5moeq49BuT_kUGJo6C3a3_vJxEMInRlc').build()
 
-    text_handler = MessageHandler(filters.TEXT, echo)
-    application.add_handler(text_handler)
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+
+    application.add_handler(MessageHandler(filters.TEXT, echo))
 
     application.run_polling()
 
--- valutes.py
+++ valutes.py
@@ -1,7 +1,8 @@
 from pprint import pprint
 import requests
 
-adres = 'https://www.cbr-xml-daily.ru/daily_json.js'
-data = requests.get(adres).json()
-pprint(data)
-# pprint(data['Valute']['USD'])
+
+def take_data():
+    adres = 'https://www.cbr-xml-daily.ru/daily_json.js'
+    data = requests.get(adres).json()
+    return data
